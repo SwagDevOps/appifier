b82784f940126043d7b856717fab9cd0f3177a90
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-06-26 18:14:47 +0200

    config (lib) add integration values

--- lib/appifier/config.rb
+++ lib/appifier/config.rb
@@ -5,6 +5,8 @@ require_relative '../appifier'
 # Config from environment variables.
 class Appifier::Config < Hash
   autoload(:YAML, 'yaml')
+  autoload(:Etc, 'etc')
+  autoload(:Pathname, 'pathname')
 
   def initialize(from: ENV, prefix: 'APPIFIER')
     @prefix = prefix
@@ -28,13 +30,24 @@ class Appifier::Config < Hash
     def defaults
       # @formatter:off
       {
-        'cache_dir' => nil,
-        'recipes_dir' => Pathname.new(__dir__).realpath.join('recipes').to_s.freeze
-      }
+        cache_dir: nil,
+        recipes_dir: Pathname.new(__dir__).realpath.join('recipes').to_s.freeze,
+        # integration values --------------------------------------------------
+        applications_dir: home.join('Applications'),
+        bin_dir: home.join('.local', 'bin'),
+        desktops_dir: home.join('.local', 'share', 'applications'),
+      }.transform_keys { |k| k.to_s.freeze }
       # @formatter:on
     end
 
     protected
+
+    # Get home directory for current user.
+    #
+    # @return [Pathname]
+    def home
+      Pathname.new(Etc.getpwnam(Etc.getlogin).dir)
+    end
 
     # @api private
     #
@@ -43,6 +56,7 @@ class Appifier::Config < Hash
       {}.tap do |result|
         source.to_h.dup.freeze.each do |k, v|
           /^#{prefix}_/.tap do |regex|
+            # noinspection RubyResolve
             result[k.gsub(regex, '').downcase] = YAML.safe_load(v) if k.match(/^#{prefix}_/)
           end
         end
