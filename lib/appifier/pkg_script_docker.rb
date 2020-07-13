4b515d60024eb059c4a7d2159b847daa99fe21c1
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-07-13 23:20:45 +0200

    pkg_script_docker (lib) replacements introudced

--- lib/appifier/pkg_script_docker.rb
+++ lib/appifier/pkg_script_docker.rb
@@ -9,6 +9,18 @@ class Appifier::PkgScriptDocker < Appifier::DownloadableString
       'https://raw.githubusercontent.com/AppImage/pkg2appimage/master/pkg2appimage-with-docker'
     end
 
+    def replacements
+      # @formatter:off
+      {
+        /^(\s*)(\$ARGS)\s*\\/ => [
+          '\\1-v "$(readlink -f functions.sh):/workspace/functions.sh:ro" \\',
+          '\\1-e ARCH="${ARCH:-x86_64}" \\',
+          '\\1\\2 \\',
+        ].join("\n"),
+      }
+      # @formatter:on
+    end
+
     def executable?
       true
     end
