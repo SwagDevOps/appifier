9edbb275ce1f197b6379778613f1b1fb28ddf3cc
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-07-14 18:35:57 +0200

    pkg_script (lib) add replacements

--- lib/appifier/pkg_script.rb
+++ lib/appifier/pkg_script.rb
@@ -9,8 +9,30 @@ class Appifier::PkgScript < Appifier::DownloadableString
       'https://raw.githubusercontent.com/AppImage/pkg2appimage/master/pkg2appimage'
     end
 
+    def replacements
+      # @formatter:off
+      {
+        /^(\s*)if \[ ! -z \$ARCH\]/ => '\\1if [ ! -z "$ARCH" ]',
+        /^(\s*OPTIONS="\$OPTIONS -o APT::Architecture=)\$ARCH"/ => "\\1#{apt_arch}\""
+      }
+      # @formatter:on
+    end
+
     def executable?
       true
+    end
+
+    protected
+
+    # @api private
+    #
+    # @return [Symbol]
+    def apt_arch
+      Appifier::Config.new.fetch('build_arch').yield_self do |s|
+        return :amd64 if ['x86_64'].include?(s)
+
+        raise 'Add arch to match deb architectures (amd64 arm64 armhf i386 powerpc ppc64el)'
+      end
     end
   end
 end
