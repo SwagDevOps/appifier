cf07c3f39aad94c606b8dc6d80223342797892fb
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-07-16 08:38:55 +0200

    helper (spec) minor changes

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,27 +1,30 @@
 # frozen_string_literal: true
 
-Dir.glob("#{__dir__}/../lib/*.rb").map { |req| require req }
+[:Appifier, 'appifier'].tap do |args|
+  # noinspection RubyResolve
+  Dir.glob("#{__dir__}/../lib/*.rb").map { |req| require req }
 
-if Gem::Specification.find_all_by_name('sys-proc').any?
-  require 'sys/proc'
+  if Gem::Specification.find_all_by_name('sys-proc').any?
+    require 'sys/proc'
 
-  Sys::Proc.progname = 'rspec'
+    Sys::Proc.progname = 'rspec'
+  end
+
+  require_relative('helper/local').tap do
+    Object.class_eval { include Local }
+  end
+
+  # @formmatter:off
+  [
+    :constants,
+    :configure,
+  ].each do |req|
+    require_relative '%<dir>s/%<req>s' % {
+      dir: __FILE__.gsub(/\.rb$/, ''),
+      req: req.to_s,
+    }
+  end
+  # @formmatter:on
+
+  autoload(*args)
 end
-
-require_relative('helper/local').tap do
-  Object.class_eval { include Local }
-end
-
-# @formmatter:off
-[
-  :constants,
-  :configure,
-].each do |req|
-  require_relative '%<dir>s/%<req>s' % {
-    dir: __FILE__.gsub(/\.rb$/, ''),
-    req: req.to_s,
-  }
-end
-# @formmatter:on
-
-autoload(:Appifier, 'appifier')
