5ca74e2f2ec6b51eb501939b81ee3b86babadc56
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-07-17 11:54:17 +0200

    container (lib) add fs + template

--- lib/appifier/container/config.rb
+++ lib/appifier/container/config.rb
@@ -4,5 +4,22 @@
 {
   config: -> { Appifier::Config.new },
   verbose: false,
+  fs: lambda do
+    autoload(:FileUtils, 'fileutils')
+
+    self[:verbose] ? FileUtils::Verbose : FileUtils
+  end,
+  template: lambda do
+    # @see https://github.com/Shopify/liquid/blob/70c45f8cd84c753298dd47488b85169458692875/README.md
+    lambda do |str, variables = {}|
+      autoload(:Liquid, 'liquid')
+
+      Liquid::Template.parse(str, { error_mode: :strict }).yield_self do |template|
+        template.render(variables, { strict_variables: true }).tap do
+          raise template.errors.first unless template.errors.empty?
+        end
+      end
+    end
+  end
 }
 # @formatter:on
