f317b0de8d9bb62034f46b95a497b5ad71c408b8
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-08-04 22:17:48 +0200

    scripts/runner (spec) add examples on sequence

--- spec/appifier/scripts/runner_spec.rb
+++ spec/appifier/scripts/runner_spec.rb
@@ -4,4 +4,19 @@ describe Appifier::Scripts::Runner, :'appifier/scripts/runner' do
   it { expect(described_class).to respond_to(:new).with_any_keywords }
   # instance methods ----------------------------------------------------------
   it { expect(subject).to respond_to(:call).with(1).arguments.and_keywords(:docker) }
+
+  # @formatter:off
+  {
+    false => [Appifier::Scripts::Pkg2appimage, Appifier::Scripts::FunctionsSh],
+    true => [
+      Appifier::Scripts::Pkg2appimageWithDocker,
+      Appifier::Scripts::Dockerfile,
+      Appifier::Scripts::Pkg2appimage,
+      Appifier::Scripts::FunctionsSh
+    ],
+  }.each do |k, v| # @formatter:on
+    context "#sequence(#{k.inspect})" do
+      it { expect(subject.__send__(:sequence, k)).to eq(v) }
+    end
+  end
 end
