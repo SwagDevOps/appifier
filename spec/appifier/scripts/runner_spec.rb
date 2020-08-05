afdb0bbf4c8060addf6620ff0a3f567d702543de
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-08-05 13:45:25 +0200

    scripts (spec) runner minor changes

--- spec/appifier/scripts/runner_spec.rb
+++ spec/appifier/scripts/runner_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 describe Appifier::Scripts::Runner, :'appifier/scripts/runner' do
-  it { expect(described_class).to respond_to(:new).with_any_keywords }
   # instance methods ----------------------------------------------------------
   it { expect(subject).to respond_to(:call).with(1).arguments.and_keywords(:docker) }
 
@@ -22,6 +21,7 @@ describe Appifier::Scripts::Runner, :'appifier/scripts/runner' do
 
   # inject --------------------------------------------------------------------
   it { expect(subject).to be_a(Appifier::Mixins::Inject) }
+  it { expect(described_class).to respond_to(:new).with_any_keywords }
 
   context '#logged_runner' do
     it { expect(subject.__send__(:logged_runner)).to be_a(Appifier::LoggedRunner) }
