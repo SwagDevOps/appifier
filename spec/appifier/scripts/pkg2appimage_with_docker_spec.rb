571cfa9246f2672212e704b1b2d530c0cedb79da
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-08-05 13:17:35 +0200

    scripts (spec) pkg2appimage_with_docker add some examples:
    * code coverage
    * check .to_path method result

--- spec/appifier/scripts/pkg2appimage_with_docker_spec.rb
+++ spec/appifier/scripts/pkg2appimage_with_docker_spec.rb
@@ -1,9 +1,43 @@
 # frozen_string_literal: true
 
+autoload(:Pathname, 'pathname')
+
 describe Appifier::Scripts::Pkg2appimageWithDocker, :'appifier/scripts/pkg2appimage_with_docker' do
   # class methods -------------------------------------------------------------
   it { expect(described_class).to respond_to(:url).with(0).arguments }
   it { expect(described_class).to respond_to(:executable?).with(0).arguments }
+
+  context '.executable?' do
+    it { expect(described_class.executable?).to be(true) }
+  end
+
+  context '.url' do
+    it { expect(described_class.url).to be_a(String) }
+  end
+
+  context '.replacements' do
+    it { expect(described_class.replacements).to be_a(Hash) }
+  end
+end
+
+describe Appifier::Scripts::Pkg2appimageWithDocker, :'appifier/scripts/pkg2appimage_with_docker' do
+  sham(:downloadable_strings).samples['pkg2appimage-with-docker'].tap do |sample|
+    let(:fake_class) { sample.builder.call }
+    let(:subject) { fake_class.new }
+    let(:output) { sample.output }
+  end
+
+  # Ensure builder has replaced `url` class method
+  { '.url': :fake_class, '#url': :subject }.each do |k, v|
+    context k do
+      it { expect(__send__(v).url).to match(%r{^file://}) }
+    end
+  end
+
+  # Compare sample with expected output
+  context '#to_s' do
+    it { expect(subject.to_s).to eq(output.read) }
+  end
 end
 
 describe Appifier::Scripts::Pkg2appimageWithDocker, :'appifier/scripts/pkg2appimage_with_docker' do
@@ -17,19 +51,22 @@ describe Appifier::Scripts::Pkg2appimageWithDocker, :'appifier/scripts/pkg2appim
     it { expect(subject.verbose?).to be(false) }
   end
 
-  # Ensure builder has replaced `url` class method
-  { '.url': :fake_class, '#url': :subject }.each do |k, v|
-    context k do
-      it { expect(__send__(v).url).to match(%r{^file://}) }
+  context '#executable?' do
+    it { expect(subject.executable?).to eq(described_class.executable?) }
+  end
+
+  context '#url' do
+    it { expect(subject.url).to eq(fake_class.url) }
+  end
+
+  context '#to_path' do
+    it { expect(subject.to_path).to be_a(String) }
+
+    it do
+      'input'.tap do |fname|
+        expect(subject.to_path).to match(%r{/#{fname}$})
+        expect(subject.to_path).to eq(Pathname.new(Dir.pwd).join(fname).to_s)
+      end
     end
-  end
-
-  context '.executable?' do
-    it { expect(fake_class.executable?).to be(true) }
-  end
-
-  # Compare sample with expected output
-  context '#to_s' do
-    it { expect(subject.to_s).to eq(output.read) }
   end
 end
