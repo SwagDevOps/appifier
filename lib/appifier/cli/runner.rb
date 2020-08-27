257e0296d9a66135f2e6b29c1352003dd46e050b
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-08-27 12:03:09 +0200

    runner (cli) use Appifier::Mixins::Inject for better IoC + add config accessor

--- lib/appifier/cli/runner.rb
+++ lib/appifier/cli/runner.rb
@@ -4,40 +4,61 @@ require_relative '../cli'
 
 # Runner
 class Appifier::Cli::Runner < Appifier::BaseCli::Runner
-  include(Appifier::Mixins::Printer)
+  include(Appifier::Mixins::Inject)
 
-  def initialize(options = {}, container: Appifier.container)
-    self.container = container
+  def initialize(options = {}, **kwargs)
+    # @formatter:off
+    {
+      config: kwargs[:config],
+      printer: kwargs[:printer],
+    }.yield_self { |injection| inject(**injection) }.assert { !values.include?(nil) }
+    # @formatter:on
 
-    super(options).tap do
-      @options = self.options
-      container.freeze
+    (kwargs[:container] || Appifier.container).yield_self do |container|
+      super(self.class.__send__(:optionize, container, options)).tap { container.freeze }
     end
   end
 
+  # @return [Appifier::Config]
+  attr_reader :config
+
+  # @return [Array<Pathname>]
   def build(recipe)
-    Appifier::Builder.new(recipe, **options).call.tap { |build| printer.call(build) }
+    Appifier::Builder.new(recipe, **options).call
+  end
+
+  # Call public method and format with printer output.
+  #
+  # @return [Object]
+  def call(method, *args)
+    super.tap { |result| printer.call(result) }
   end
 
   protected
 
-  # @return [Appifier::Container]
-  attr_accessor :container
+  # @return [Appifier::JsonPrinter]
+  attr_reader :printer
 
-  # Prepare options.
-  #
-  # Pass some options from options to container values,
-  # as a result options are removed.
-  #
-  # @return [self]
-  def options
-    return super if container.frozen?
+  class << self
+    # Prepare options from given container.
+    #
+    # Pass some options from options to container values,
+    # as a result options are removed.
+    #
+    # @param [Appifier::Conatiner] container
+    # @param [Hash] options
+    #
+    # @return [Hash]
+    # @api private
+    def optionize(container, options = {})
+      return options if container.frozen?
 
-    super.transform_keys(&:to_sym).dup.tap do |options|
-      # k: container key, v: options key
-      { verbose: :verbose }.each do |k, v|
-        -> { options.delete(k) }.tap { container[v] = options[k] }.call
-      end
-    end.freeze
+      options.transform_keys(&:to_sym).dup.tap do |options| # rubocop:disable Lint/ShadowingOuterLocalVariable
+        # k: container key, v: options key
+        { verbose: :verbose }.each do |k, v|
+          -> { options.delete(k) }.tap { container[v] = options[k] }.call
+        end
+      end.freeze
+    end
   end
 end
