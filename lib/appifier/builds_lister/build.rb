56b4cddafa2f7cd1b8a93e00177eeed797b4bddc
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-09-01 09:10:39 +0200

    build (builds_lister) introduce #extractable? method

--- lib/appifier/builds_lister/build.rb
+++ lib/appifier/builds_lister/build.rb
@@ -44,15 +44,27 @@ class Appifier::BuildsLister::Build
 
   protected
 
+  # Denote name and version can be extracted from path.
+  #
+  # @return [Boolean]
+  # @api private
+  def extractable?
+    !to_s.scan(version_matcher).to_a.empty?
+  end
+
   # @return [Regexp]
   def version_matcher
-    %r{^#{path.dirname}/(.*)-([0-9]+.*).glibc}
+    %r{^#{path.dirname}/(.*)-(([0-9]+.*)|).glibc}
   end
 
   # @return [Struct]
   def extract
-    to_s.scan(version_matcher).to_a.fetch(0).yield_self do |r|
-      Struct.new(:name, :version).new(r.fetch(0), r[1])
+    Struct.new(:name, :version).yield_self do |result_struct|
+      return result_struct.new(nil, nil) unless extractable?
+
+      to_s.scan(version_matcher).to_a.fetch(0).yield_self do |r|
+        result_struct.new(r.fetch(0), r[1].to_s.empty? ? nil : r[1].to_s)
+      end
     end
   end
 end
