42594f669c5e9d2fc97fa0b05cd36ee4f6040161
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-09-01 17:58:45 +0200

    jsonable (mixins) minor changes

--- lib/appifier/mixins/jsonable.rb
+++ lib/appifier/mixins/jsonable.rb
@@ -4,12 +4,20 @@ require_relative '../mixins'
 
 # Convert the object to its JSON representation.
 module Appifier::Mixins::Jsonable
+  class << self
+    protected
+
+    def included(othermod)
+      super
+
+      require 'json'
+    end
+  end
+
   # @see https://ruby-doc.org/stdlib-2.6.3/libdoc/json/rdoc/JSON.html#module-JSON-label-Generating+JSON
   #
   # @return [String]
   def to_json(*args)
-    require 'json'
-
     super(*args)
   end
 end
