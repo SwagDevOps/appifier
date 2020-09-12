8f739c68f16c08265a4826c7d43d08ecb29f2ae9
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-09-12 19:21:47 +0200

    scan_result (builds_lister) #catalog returns an HashGlob mixin

--- lib/appifier/builds_lister/scan_result.rb
+++ lib/appifier/builds_lister/scan_result.rb
@@ -17,20 +17,21 @@ class Appifier::BuildsLister::ScanResult < ::Hash
   # Get builds indexed by names with versions.
   #
   # Versions are used as indexes.
-  # Get only arrays of versions, when ``with_details`` is false.
+  # Get only arrays of versions, when ``with_details`` is ``false``.
   #
+  # @return [Appifier::Mixins::HashGlob]
   # @return [Hash{String => Hash{String => Appifier::BuildsLister::Build}}]
   # @return [Hash{String => Array<String>}]
-  def catalog(with_details: true)
+  def catalog(with_details: true) # rubocop:disable Metrics/AbcSize
     {}.tap do |h|
       self.each do |k, v|
         h[k] ||= {}
-        v.each do |build|
-          h[k][build.version.freeze] = build.freeze
-        end
+        v.each { |build| h[k][build.version.freeze] = build.freeze }
       end
 
       h.each { |k, v| h[k] = v.keys.freeze } unless with_details
-    end.sort.to_h.transform_values(&:freeze)
+    end.sort.to_h.transform_values(&:freeze).tap do |instance|
+      instance.singleton_class.__send__(:include, Appifier::Mixins::HashGlob)
+    end
   end
 end
