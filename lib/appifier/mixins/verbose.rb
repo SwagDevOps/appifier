58bd72eae6feedf1e02d228499fddd633456767e
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-09-18 13:08:09 +0200

    verbose (mixins) major rewrite preserving IoC possibility and ease of use

--- lib/appifier/mixins/verbose.rb
+++ lib/appifier/mixins/verbose.rb
@@ -4,10 +4,17 @@ require_relative '../mixins'
 
 # Verbose behaviour.
 module Appifier::Mixins::Verbose
+  # Use ``Appifier.container`` except when a ``verbose`` method is present.
+  #
   # @return [Boolean]
   def verbose?
-    return !!@verbose unless @verbose.nil?
-
-    (self.respond_to?(:container) ? container : Appifier.container).resolve(:verbose)
+    (self.methods + self.private_methods).map(&:to_sym).include?(:verbose).yield_self do |b|
+      # @formatter:off
+      {
+        true => -> { self.__send__(:verbose) },
+        false => -> { Appifier.container.resolve(:verbose) }
+      }.fetch(b).call.yield_self { |result| !!result }
+      # @formatter:on
+    end
   end
 end
