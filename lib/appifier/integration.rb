d6429d333dc23cc63796c0f3cb4c4bb5c644b3e2
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-10-06 20:00:53 +0200

    integration (lib) deep_merge integration configs

--- lib/appifier/integration.rb
+++ lib/appifier/integration.rb
@@ -74,6 +74,29 @@ class Appifier::Integration
 
   protected
 
+  class << self
+    # @param [Hash] origin
+    # @param [Hash] merged
+    #
+    # @api private
+    #
+    # @return [Hash]
+    # #raise [ArgumentError]
+    # @see Appifier::Integration#parameterize
+    def deep_merge(origin, merged)
+      unless origin.is_a?(Hash) and merged.is_a?(Hash)
+        "Expected Hash arguments, got #{[origin.class, merged.class]}"
+          .tap { |message| raise ArgumentError, message }
+      end
+
+      lambda do |_, x, y|
+        y.tap do
+          return deep_merge(x, y) if x.is_a?(Hash) and y.is_a?(Hash)
+        end
+      end.yield_self { |f| origin.merge(merged, &f) }
+    end
+  end
+
   # @return [Pathname]
   attr_reader :build
 
@@ -83,14 +106,16 @@ class Appifier::Integration
   # @param [Hash|Appifier::Recipe] recipe
   #
   # @return [Hash]
-  def parameterize(recipe)
+  def parameterize(recipe) # rubocop:disable Metrics/AbcSize
     { # @formatter:off
       name: recipe.to_h.fetch('app').to_s,
       executable: recipe.to_h.fetch('app').to_s.downcase,
       logname: recipe.to_s,
       exec_params: [], # @formatter:on
     }.transform_keys(&:to_s).merge(recipe.to_h.fetch('integration', {})).yield_self do |h|
-      h.merge(user_integrations.fetch(h.fetch('name'), {}))
-    end.transform_values(&:freeze).freeze
+      self.class.__send__(:deep_merge, h, user_integrations[h.fetch('name')].to_h)
+          .to_h
+          .transform_values(&:freeze).freeze
+    end
   end
 end
