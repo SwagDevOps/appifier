43080de9e69c14be4c91f33bb9e31372fe506d81
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-12-31 12:42:47 +0100

    runner (scripts) better IoC using ``scripts.sequencer`` + refactor #call into smaller methods

--- lib/appifier/scripts/runner.rb
+++ lib/appifier/scripts/runner.rb
@@ -11,30 +11,21 @@ class Appifier::Scripts::Runner
     {
       logged_runner: kwargs[:logged_runner],
       config: kwargs[:config],
+      scripts_sequencer: [kwargs[:sequencer], :'scripts.sequencer'],
     }.yield_self do |injection|
       inject(**injection).assert { !values.include?(nil) }
     end
   end
 
-  # @param [Appifier::Recipe] recipe
+  # @param recipe [Appifier::Recipe]
+  # @param docker [Boolean]
   #
   # @see Appifier::Builder#build
-  def call(recipe, docker:) # rubocop:disable Metrics/MethodLength
-    {
-      'DOCKER_APP_NAME' => recipe.to_h.fetch('app'),
-      'DOCKER_CACHING' => config.fetch('docker_caching'),
-    }.transform_values(&:to_s).tap do |env|
-      Appifier::Scripts::Sequence.new(docker).start.tap do |scripts|
-        {
-          recipe.name.to_s => [
-            [
-              env,
-              scripts.fetch(0).to_path,
-              recipe.to_path,
-            ]
-          ]
-        }.yield_self { |definitions| logged_runner.call(definitions) }
-      end
+  #
+  # @return [Array<Appifier::DownloadableString>]
+  def call(recipe, docker:)
+    scripts_sequencer.call(docker).tap do |scripts|
+      make_runner_definitions(scripts, recipe: recipe).yield_self { |definitions| logged_runner.call(definitions) }
     end
   end
 
@@ -46,4 +37,38 @@ class Appifier::Scripts::Runner
   # @return [Hash]
   # @return [Appifier::Config]
   attr_reader :config
+
+  # @return [Class<Appifier::Scripts::Sequence>]
+  attr_reader :scripts_sequencer
+
+  # @api private
+  #
+  # @param recipe [Appifier::Recipe]
+  #
+  # @return [Hash{String => String}]
+  def make_env(recipe:)
+    # noinspection RubyYardReturnMatch
+    {
+      'DOCKER_APP_NAME' => recipe.to_h.fetch('app'),
+      'DOCKER_CACHING' => config.fetch('docker_caching'),
+    }.transform_values(&:to_s)
+  end
+
+  # Make definitions used by ``logged_runner``.
+  #
+  # @param scripts [Array<Appifier::DownloadableString>]
+  # @param recipe [Appifier::Recipe]
+  #
+  # @return [Hash{String => Array}]
+  def make_runner_definitions(scripts, recipe:)
+    {
+      recipe.name.to_s => [
+        [
+          make_env(recipe: recipe),
+          scripts.fetch(0).to_path,
+          recipe.to_path,
+        ]
+      ]
+    }
+  end
 end
