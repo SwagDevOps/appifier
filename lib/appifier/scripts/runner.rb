c6dc55f6f06a8d97280931639ae08220475480a4
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-12-31 11:36:04 +0100

    runner (scripts) recipe passed as oath to pkg2appimage scripts

--- lib/appifier/scripts/runner.rb
+++ lib/appifier/scripts/runner.rb
@@ -8,29 +8,32 @@ class Appifier::Scripts::Runner
   include(Appifier::Mixins::Inject)
 
   def initialize(**kwargs)
-    # @formatter:off
     {
       logged_runner: kwargs[:logged_runner],
       config: kwargs[:config],
     }.yield_self do |injection|
-      # @formatter:on
       inject(**injection).assert { !values.include?(nil) }
     end
   end
 
   # @param [Appifier::Recipe] recipe
-  def call(recipe, docker:)
-    # @formatter:off
+  #
+  # @see Appifier::Builder#build
+  def call(recipe, docker:) # rubocop:disable Metrics/MethodLength
     {
       'DOCKER_APP_NAME' => recipe.to_h.fetch('app'),
       'DOCKER_CACHING' => config.fetch('docker_caching'),
     }.transform_values(&:to_s).tap do |env|
-      # @formatter:on
       Appifier::Scripts::Sequence.new(docker).start.tap do |scripts|
-        # @formatter:off
         {
-          recipe.name.to_s => [[env, scripts.fetch(0).to_path, recipe.name.to_s]]
-        }.yield_self { |definitions| logged_runner.call(definitions) } # @formatter:on
+          recipe.name.to_s => [
+            [
+              env,
+              scripts.fetch(0).to_path,
+              recipe.to_path,
+            ]
+          ]
+        }.yield_self { |definitions| logged_runner.call(definitions) }
       end
     end
   end
